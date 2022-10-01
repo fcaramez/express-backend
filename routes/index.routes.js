7bfb6418753277d2f08e55c1d1224b2c01759afa
fcaramez <francisco.caramez.duarte@gmail.com>
2022-10-01 10:30:20 +0100

    adds cloudinary setup

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+const fileUploader = require("../config/cloudinary.config");
 
 router.get("/ping", (req, res, next) => {
   res.json("Server up and running");
 });
+
+router.post(
+  "/upload",
+  isAuthenticated,
+  fileUploader.single("image"),
+  (req, res, next) => {
+    try {
+      if (!req.file) {
+        next(new Error("No file uploaded!"));
+        return;
+      }
+
+      res.json({ fileUrl: req.file.path });
+    } catch (error) {}
+  }
+);
 
 router.use("/auth", authRoutes);
 
